5f7ca478732aadacd905aa32cc044eb15e4f1d66
Łukasz Kręc <92949106+lukaszkrec@users.noreply.github.com>
2023-05-22 09:18:04 +0200

    Update script.js

--- script.js
+++ script.js
@@ -80,58 +80,60 @@ function getStream(type) {
 }
 
 window.SpeechRecognition = window.SpeechRecognition       ||
-window.webkitSpeechRecognition ||
-null;
+                                 window.webkitSpeechRecognition ||
+                                 null;
 
-if (window.SpeechRecognition === null) {
-document.getElementById('ws-unsupported').classList.remove('hidden');
-document.getElementById('button-play-ws').setAttribute('disabled', 'disabled');
-document.getElementById('button-stop-ws').setAttribute('disabled', 'disabled');
-} else {
-var recognizer = new window.SpeechRecognition();
-var transcription = document.getElementById('transcription');
-var log = document.getElementById('log');
+      if (window.SpeechRecognition === null) {
+        document.getElementById('ws-unsupported').classList.remove('hidden');
+        document.getElementById('button-play-ws').setAttribute('disabled', 'disabled');
+        document.getElementById('button-stop-ws').setAttribute('disabled', 'disabled');
+      } else {
+        var recognizer = new window.SpeechRecognition();
+        var transcription = document.getElementById('transcription');
+        var log = document.getElementById('log');
 
-// Recogniser doesn't stop listening even if the user pauses
-recognizer.continuous = true;
+        // Recogniser doesn't stop listening even if the user pauses
+        recognizer.continuous = true;
 
-// Start recognising
-recognizer.onresult = function(event) {
-transcription.textContent = '';
+        // Start recognising
+        recognizer.onresult = function(event) {
+          transcription.textContent = '';
 
-for (var i = event.resultIndex; i < event.results.length; i++) {
-if (event.results[i].isFinal) {
-transcription.textContent = event.results[i][0].transcript + ' (Confidence: ' + event.results[i][0].confidence + ')';
-} else {
-transcription.textContent += event.results[i][0].transcript;
-}
-}
-};
+          for (var i = event.resultIndex; i < event.results.length; i++) {
+            if (event.results[i].isFinal) {
+              transcription.textContent = event.results[i][0].transcript + ' (Confidence: ' + event.results[i][0].confidence + ')';
+              document.body.style.background = event.results[i][0].transcript;
+            } else {
+              transcription.textContent += event.results[i][0].transcript;
+              document.body.style.background = event.results[i][0].transcript;
+            }
+          }
+        };
 
-// Listen for errors
-recognizer.onerror = function(event) {
-log.innerHTML = 'Recognition error: ' + event.message + '<br />' + log.innerHTML;
-};
+        // Listen for errors
+        recognizer.onerror = function(event) {
+          log.innerHTML = 'Recognition error: ' + event.message + '<br />' + log.innerHTML;
+        };
 
-document.getElementById('button-play-ws').addEventListener('click', function() {
-// Set if we need interim results
-recognizer.interimResults = document.querySelector('input[name="recognition-type"][value="interim"]').checked;
+        document.getElementById('button-play-ws').addEventListener('click', function() {
+          // Set if we need interim results
+          recognizer.interimResults = document.querySelector('input[name="recognition-type"][value="interim"]').checked;
 
-try {
-recognizer.start();
-log.innerHTML = 'Recognition started' + '<br />' + log.innerHTML;
-} catch(ex) {
-log.innerHTML = 'Recognition error: ' + ex.message + '<br />' + log.innerHTML;
-}
-});
+          try {
+            recognizer.start();
+            log.innerHTML = 'Recognition started' + '<br />' + log.innerHTML;
+          } catch(ex) {
+            log.innerHTML = 'Recognition error: ' + ex.message + '<br />' + log.innerHTML;
+          }
+        });
 
-document.getElementById('button-stop-ws').addEventListener('click', function() {
-recognizer.stop();
-log.innerHTML = 'Recognition stopped' + '<br />' + log.innerHTML;
-document.body.style.background = event.results[i][0].transcript;
-});
+        document.getElementById('button-stop-ws').addEventListener('click', function() {
+          recognizer.stop();
+          log.innerHTML = 'Recognition stopped' + '<br />' + log.innerHTML;
+        });
 
-document.getElementById('clear-all').addEventListener('click', function() {
-transcription.textContent = '';
-log.textContent = '';
-})}
+        document.getElementById('clear-all').addEventListener('click', function() {
+          transcription.textContent = '';
+          log.textContent = '';
+        });
+      }
